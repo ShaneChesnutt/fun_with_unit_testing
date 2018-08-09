822dc83efa1bf09c5449c29aecacd8e68af2ec29
Shane Chesnutt <shanechesnutt@gmail.com>
2018-08-09 00:00:36 -0400

    Add tests for shovel class

--- src/__tests__/shovel_spec.js
+++ src/__tests__/shovel_spec.js
@@ -2,8 +2,44 @@ describe('shovel', () => {
   const Shovel = require('../shovel');
   let subject;
 
-  it('sample test', () => {
-    expect(true).toBe(true);
+  describe('constructor', () => {
+    beforeEach(() => {
+      subject = new Shovel();
+    });
+
+    it('sets the thrustDamage to 10', () => {
+      expect(subject.thrustDamage).toEqual(10);
+    });
+
+    it('sets the slashDamage to 10', () => {
+      expect(subject.thrustDamage).toEqual(10);
+    });
+  });
+
+  describe('thrustAttack', () => {
+    const expected = 10;
+    let actual;
+    beforeEach(() => {
+      subject = new Shovel();
+      actual = subject.thrustAttack();
+    });
+
+    it('returns thrustDamage', () => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('slashAttack', () => {
+    const expected = 10;
+    let actual;
+    beforeEach(() => {
+      subject = new Shovel();
+      actual = subject.slashAttack();
+    });
+
+    it('returns slashDamage', () => {
+      expect(actual).toEqual(expected);
+    });
   });
 });
 
