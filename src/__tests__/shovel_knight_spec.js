df636ffdc9f09932b278154a6887a0154c5cc460
Shane Chesnutt <shanechesnutt@gmail.com>
2018-08-08 23:50:30 -0400

    Add tests for shovel_knight class

--- src/__tests__/shovel_knight_spec.js
+++ src/__tests__/shovel_knight_spec.js
@@ -61,4 +61,105 @@ describe('ShovelKnight', () => {
       });
     });
   });
+
+  describe('equipArmor', () => {
+    const armor = {
+      rating: 10
+    };
+
+    beforeEach(() => {
+      subject = new ShovelKnight();
+      subject.equipArmor(armor);
+    });
+
+    it('sets armor to the passed in armor', () => {
+      expect(subject.armor).toEqual(armor);
+    });
+  });
+
+  describe('unequipArmor', () => {
+    const armor = {
+      rating: 10
+    };
+
+    beforeEach(() => {
+      subject = new ShovelKnight(armor);
+      subject.unequipArmor();
+    });
+
+    it('sets armor to null', () => {
+      expect(subject.armor).toBeNull();
+    });
+  });
+
+  describe('thrustAttack', () => {
+    const thrustAttackDamage = 10,
+      expected = 15;
+
+    let actual;
+
+    beforeEach(() => {
+      Shovel.prototype.__setThrustDamage(thrustAttackDamage);
+      subject = new ShovelKnight();
+      actual = subject.thrustAttack();
+    });
+
+    it('returns Strength + Weapon.thrustAttack', () => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('slashAttack', () => {
+    const slashAttackDamage = 10,
+      expected = 15;
+
+    let actual;
+
+    beforeEach(() => {
+      Shovel.prototype.__setSlashDamage(slashAttackDamage);
+      subject = new ShovelKnight();
+      actual = subject.slashAttack();
+    });
+
+    it('returns Strength + Weapon.slashAttack', () => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('takeDamage', () => {
+    describe('when armor is not null', () => {
+      const armor = {
+          rating: 10
+        },
+        damage = 100,
+        expected = 85;
+
+      let actual;
+
+      beforeEach(() => {
+        subject = new ShovelKnight(armor);
+        actual = subject.takeDamage(damage);
+      });
+
+      it('returns damage - ( fortitude + armor.rating )', () => {
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe('when armor is null', () => {
+      const damage = 100,
+        expected = 95;
+
+      let actual;
+
+      beforeEach(() => {
+        subject = new ShovelKnight();
+        actual = subject.takeDamage(damage);
+      });
+
+      it('returns damage - fortitude', () => {
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
 });
